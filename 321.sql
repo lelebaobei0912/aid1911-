0efe3d70e40854ae915ad40be759213f7d479fb1
Tedu <525923741@qq.com>
2020-01-16 10:48:46 +0800

    init commit

---
+++ 321.sql
@@ -0,0 +1,23 @@
+create table syudent(
+s_id varchar(10),
+s_name varchar(20),
+s_age date,
+s_sex varchar(10),
+)
+
+create table score(
+s_id varchar(10),
+coerse_id varchar(10),
+s_score varchar(10)
+)
+
+create table course(
+c_id varchar(10),
+c_name varchar(20),
+teachar_id varchar(10)
+)
+
+create table tearchar(
+t_id varchar(10),
+t_name varchar(20)
+)
